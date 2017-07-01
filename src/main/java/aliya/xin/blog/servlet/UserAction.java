f6b1251a07276b57211bce3dd6de5d9401ebc48c
Tony.Jaa <1291813139@qq.com>
2017-07-01 11:05:26 +0800

    blog

--- src/main/java/aliya/xin/blog/servlet/UserAction.java
+++ src/main/java/aliya/xin/blog/servlet/UserAction.java
@@ -1,22 +1,81 @@
 package aliya.xin.blog.servlet;
 
+import aliya.xin.blog.util.Db;
+import aliya.xin.blog.util.Error;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * Created by zhenya.1291813139.com
  * on 2017/7/1.
  * blog.
  */
+@WebServlet(urlPatterns = "/user")
 public class UserAction extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String action = req.getParameter("action");
+
+        if ("signUp".equals(action)) {
+            signUp(req,resp);
+            return;
+        }
+
+        Error.showError(req,resp);
     }
 
+    private void signUp(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String email = req.getParameter("email").trim();
+        String plainPassword = req.getParameter("password");
+
+        if (email.length() == 0) {
+            req.setAttribute("message", "请输入邮箱地址");
+            return;
+        }
+        if (plainPassword.length() < 6) {
+            req.setAttribute("message", "密码不能少于6个字符");
+            req.getRequestDispatcher("sign_in.jsp").forward(req, resp);
+            return;
+        }
+
+        StrongPasswordEncryptor encryptor = new StrongPasswordEncryptor();
+        String password = encryptor.encryptPassword(plainPassword);
+
+        Connection connection = Db.getConnection();
+        PreparedStatement preparedStatement = null;
+
+        String sql = "INSERT INTO db_blog.user(email, password, signUpIp, lastIp) VALUE(?, ?, ?, ?)";
+
+        try {
+            if (connection != null) {
+                preparedStatement = connection.prepareStatement(sql);
+            } else {
+                Error.showError(req, resp);
+                return;
+            }
+            preparedStatement.setString(1, email);
+            preparedStatement.setString(2, password);
+            preparedStatement.setString(3, req.getRemoteAddr());
+            preparedStatement.setString(4, req.getRemoteAddr());
+
+            preparedStatement.executeUpdate();
+
+            resp.sendRedirect("/");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            Db.close(null, preparedStatement, connection);
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doPost(req,resp);
