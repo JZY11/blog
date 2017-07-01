8ea5a9dcbc8766c5b78a4588ff01af57c6142a41
Tony.Jaa <1291813139@qq.com>
2017-07-01 10:18:39 +0800

    blog

---
+++ src/main/java/aliya/xin/blog/servlet/UserAction.java
@@ -0,0 +1,24 @@
+package aliya.xin.blog.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by zhenya.1291813139.com
+ * on 2017/7/1.
+ * blog.
+ */
+public class UserAction extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+}
